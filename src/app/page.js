b8af9a364b5ac980ca26e80a246a12554c60d394
jaysys <jaysys@gmail.com>
2024-09-06 04:52:58 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/app/page.js
    	modified:   src/app/page.module.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ImageInfoPage from "./imageinfo/page";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <p>STRAW Api testing with React/Nextjs</p>
+      <ImageInfoPage />
     </div>
   );
 }
