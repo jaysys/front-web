3fc6114ccf34002c02b64f8d268891082830b732
jaysys <jaysys@gmail.com>
2024-09-06 03:10:08 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   backend_fastapi/main.py

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -26,7 +26,7 @@ from fastapi import FastAPI
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"STRAWAPI":f"{app.version}"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
